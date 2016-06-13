03094c03eec793d8082c39d8ca97a789c13d4b4f
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-13 23:18:19 +0900

    Create question02.

---
+++ rspec/spec/question02_spec.rb
@@ -0,0 +1,27 @@
+describe 'Mathematical calculations' do
+
+  describe 'addition' do
+    it '1 + 1 equals 2' do
+      expect(1 + 1).to eq 2
+    end
+  end
+
+  describe 'subtraction' do
+    it '10 - 1 equals 9' do
+      expect(10 - 1).to eq 9
+    end
+  end
+
+  describe 'multiplication' do
+    it '5 * 4 equals 20' do
+      expect(5 * 4).to eq 20
+    end
+  end
+
+  describe 'division' do
+    it '40 / 8 equals 5' do
+      expect(40 / 8).to eq 5
+    end
+  end
+
+end
