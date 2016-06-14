d8a262b41e0c8a97470d28e998a3416f60af3700
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-14 22:28:37 +0900

    Create question06.

---
+++ rspec/lib/question06.rb
@@ -0,0 +1,12 @@
+class Name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+
+end
