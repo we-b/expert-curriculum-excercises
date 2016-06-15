58e9b1c29406994d66f35489abe698d36ddcaa07
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-16 02:01:50 +0900

    Create question08 with factory.

---
+++ rspec/lib/question08.rb
@@ -0,0 +1,12 @@
+class Fruit
+
+  def initialize(first_fruit, second_fruit, third_fruit)
+    @my_fruits = []
+    @my_fruits.push(first_fruit, second_fruit, third_fruit)
+  end
+
+  def my_fruits
+    @my_fruits
+  end
+
+end
