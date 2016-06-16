b62105d072ded2bccf52b2bcdcf3a57a73e08be7
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-17 00:43:10 +0900

    Modify question 06 and the corresponding spec file to match question 09.

--- rspec/lib/question06.rb
+++ rspec/lib/question06.rb
@@ -1,12 +1,22 @@
 class Name
 
-  def initialize(first_name, last_name)
+  def initialize(first_name, last_name, email, company)
     @first_name = first_name
     @last_name = last_name
+    @email = email
+    @company = company
   end
 
   def full_name
     "#{@first_name} #{@last_name}"
+  end
+
+  def my_email
+    @email
+  end
+
+  def my_company
+    @company
   end
 
 end
--- rspec/spec/question06_spec.rb
+++ rspec/spec/question06_spec.rb
@@ -4,14 +4,14 @@ describe Name do
   describe '#full_name' do
     context '名前がTech Taroの場合' do
       it '正しい名前であること' do
-        user = Name.new('Tech', 'Taro')
+        user = Name.new('Tech', 'Taro', 'Tech-Taro@gmail.com', 'div')
         expect(user.full_name).to eq('Tech Taro')
       end
     end
 
     context '名前がTech Taroでない場合' do
       it '正しくない名前であること' do
-        user = Name.new('Tech', 'Jiro')
+        user = Name.new('Tech', 'Jiro', 'Tech-Jiro@gmail.com', 'div')
         expect(user.full_name).not_to eq('Tech Taro')
       end
     end
