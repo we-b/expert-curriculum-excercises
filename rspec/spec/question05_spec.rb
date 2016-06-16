546a03e93cb3513971abb1cbe5a4308b2556d9b8
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-17 00:38:04 +0900

    Modify the message.

--- rspec/spec/question05_spec.rb
+++ rspec/spec/question05_spec.rb
@@ -3,14 +3,14 @@ require 'question05'
 describe User do
   describe '#match_user?' do
     context '名前がTaroの場合' do
-      it '真を返す' do
+      it '真を返すこと' do
         user = User.new('Taro')
         expect(user.match_user?).to be_truthy
       end
     end
 
     context '名前がTaroでない場合' do
-      it '偽を返す' do
+      it '偽を返こと' do
         user = User.new('Jiro')
         expect(user.match_user?).to be_falsey
       end
