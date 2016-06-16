c8ed8e60ced3cef4a11c0fce1e8b2fd22fbab04e
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-17 00:42:23 +0900

    Create question 09.

--- rspec/spec/factories/names.rb
+++ rspec/spec/factories/names.rb
@@ -3,8 +3,18 @@ FactoryGirl.define do
   factory :name do
     first_name 'Tech'
     last_name 'Taro'
+    email 'Tech-Taro@gmail.com'
+    company 'div'
 
-    initialize_with { new(first_name, last_name) }
+    factory :email do
+      email { Faker::Internet.free_email }
+    end
+
+    factory :company do
+      company { Faker::Company.name }
+    end
+
+    initialize_with { new(first_name, last_name, email, company) }
   end
 
 end
