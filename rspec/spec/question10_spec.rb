f21d7d57353fee4f48017378a8f6d1958f6cddcf
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-17 12:55:17 +0900

    Refactor question10.

--- rspec/spec/question10_spec.rb
+++ rspec/spec/question10_spec.rb
@@ -2,15 +2,14 @@ require 'question10'
 
 describe Sort do
   describe '#self.sort_numbers' do
+    let(:numbers_in_an_array) { build(:sort) }
     context 'sort_numbersが正しい場合' do
       it '昇順にソートされた配列になること' do
-        numbers_in_an_array = build(:sort)
         expect(numbers_in_an_array.sort_numbers).to match([0, 1, 2, 6, 9])
       end
     end
     context 'sort_numbersが正しくない場合' do
       it '昇順にソートされた配列にならないこと' do
-        numbers_in_an_array = build(:sort)
         expect(numbers_in_an_array.sort_numbers).not_to match([9, 6, 2, 1, 0])
       end
     end
