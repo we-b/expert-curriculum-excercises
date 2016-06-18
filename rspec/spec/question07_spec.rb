de9d0415b5ef2d02be9f5744e7c69c660ed1ff42
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-19 00:53:05 +0900

    Refactor question07.

--- rspec/spec/question07_spec.rb
+++ rspec/spec/question07_spec.rb
@@ -11,7 +11,7 @@ describe Name do
 
     context '名前がTech Taroでない場合 (factory使用)' do
       it '正しくない名前であること' do
-        user = build(:name, first_name: 'Tech', last_name: 'Jiro')
+        user = build(:name, last_name: 'Jiro')
         expect(user.full_name).not_to eq('Tech Taro')
       end
     end
