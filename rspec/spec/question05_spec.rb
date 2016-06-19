415acad924b36978426aab50a1acd0cc14cec168
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-19 10:41:50 +0900

    Refactor question05.

--- rspec/spec/question05_spec.rb
+++ rspec/spec/question05_spec.rb
@@ -2,19 +2,41 @@ require 'question05'
 
 describe User do
   describe '#match_user?' do
+    let(:user) { User.new(name) }
     context '名前がTaroの場合' do
+      let(:name) { 'Taro' }
       it '真を返すこと' do
-        user = User.new('Taro')
         expect(user.match_user?).to be_truthy
       end
     end
 
     context '名前がTaroでない場合' do
+      let(:name) { 'Jiro' }
       it '偽を返すこと' do
-        user = User.new('Jiro')
         expect(user.match_user?).to be_falsey
       end
     end
   end
 end
+
+
+
+# describe User do
+#   describe '#match_user?' do
+#     context '名前がTaroの場合' do
+#       it '真を返すこと' do
+#         user = User.new('Taro')
+#         expect(user.match_user?).to be_truthy
+#       end
+#     end
+
+#     context '名前がTaroでない場合' do
+#       it '偽を返すこと' do
+#         user = User.new('Jiro')
+#         expect(user.match_user?).to be_falsey
+#       end
+#     end
+#   end
+# end
+
 
