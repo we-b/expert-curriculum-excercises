1d4b9f5aca144e7c8d06a9b79ff5c37ede36d5fd
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-20 22:56:23 +0900

    Simplify the code by deleting unnecessary lines.

--- rspec/spec/question07_spec.rb
+++ rspec/spec/question07_spec.rb
@@ -2,7 +2,7 @@ require 'question06'
 
 describe Name do
   describe '#full_name' do
-    it 'フルネームでを返すこと' do
+    it 'フルネームを返すこと' do
       user = build(:name)
       expect(user.full_name).to eq('Tech Taro')
     end
--- rspec/spec/question09_spec.rb
+++ rspec/spec/question09_spec.rb
@@ -2,34 +2,16 @@ require 'question06'
 
 describe Name do
   describe '#my_email' do
-    context 'emailが正しい場合' do
-      it 'Tech-Taro@gmail.comであること' do
-        user = build(:email, email: 'Tech-Taro@gmail.com')
-        expect(user.my_email).to eq('Tech-Taro@gmail.com')
-      end
-    end
-
-    context 'emailが正しく無い場合' do
-      it 'Tech-Taro@gmail.comでないこと' do
-        user = build(:email)
-        expect(user.my_email).not_to eq('Tech-Taro@gmail.com')
-      end
+    it 'Tech-Taro@gmail.comを返すこと' do
+      user = build(:email, email: 'Tech-Taro@gmail.com')
+      expect(user.email).to eq('Tech-Taro@gmail.com')
     end
   end
 
   describe '#my_company' do
-    context 'companyが正しい場合' do
-      it 'divであること' do
-        user = build(:company, company: 'div')
-        expect(user.my_company).to eq('div')
-      end
-    end
-
-    context 'companyが正しく無い場合' do
-      it 'divではないこと' do
-        user = build(:company)
-        expect(user.my_email).not_to eq('div')
-      end
+    it 'divを返すこと' do
+      user = build(:company, company: 'div')
+      expect(user.company).to eq('div')
     end
   end
 end
