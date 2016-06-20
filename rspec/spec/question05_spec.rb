94ded26d8c6e0bc33c65c33327f917ad20a03793
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-20 22:37:50 +0900

    Delete unnecessary codes.

--- rspec/spec/question05_spec.rb
+++ rspec/spec/question05_spec.rb
@@ -18,25 +18,3 @@ describe User do
     end
   end
 end
-
-
-
-# describe User do
-#   describe '#match_user?' do
-#     context '名前がTaroの場合' do
-#       it '真を返すこと' do
-#         user = User.new('Taro')
-#         expect(user.match_user?).to be_truthy
-#       end
-#     end
-
-#     context '名前がTaroでない場合' do
-#       it '偽を返すこと' do
-#         user = User.new('Jiro')
-#         expect(user.match_user?).to be_falsey
-#       end
-#     end
-#   end
-# end
-
-
