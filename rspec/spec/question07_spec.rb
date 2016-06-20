4bb6186c8ce963612a18080f783c507ba4427ee3
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-20 22:46:38 +0900

    Delete the fail test code, and simplify the code.

--- rspec/spec/question07_spec.rb
+++ rspec/spec/question07_spec.rb
@@ -2,18 +2,9 @@ require 'question06'
 
 describe Name do
   describe '#full_name' do
-    context '名前がTech Taroの場合 (factory使用)' do
-      it '正しい名前であること' do
-        user = build(:name)
-        expect(user.full_name).to eq('Tech Taro')
-      end
-    end
-
-    context '名前がTech Taroでない場合 (factory使用)' do
-      it '正しくない名前であること' do
-        user = build(:name, last_name: 'Jiro')
-        expect(user.full_name).not_to eq('Tech Taro')
-      end
+    it 'フルネームでを返すこと' do
+      user = build(:name)
+      expect(user.full_name).to eq('Tech Taro')
     end
   end
 end
