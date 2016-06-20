cc88dfe976ad3aec8a5fec4fcda1b71567944b0a
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-20 22:44:28 +0900

    Delete a test that fails, and simplify the test.

--- rspec/spec/question06_spec.rb
+++ rspec/spec/question06_spec.rb
@@ -2,18 +2,9 @@ require 'question06'
 
 describe Name do
   describe '#full_name' do
-    context '名前がTech Taroの場合' do
-      it '正しい名前であること' do
-        user = Name.new('Tech', 'Taro', 'Tech-Taro@gmail.com', 'div')
-        expect(user.full_name).to eq('Tech Taro')
-      end
-    end
-
-    context '名前がTech Taroでない場合' do
-      it '正しくない名前であること' do
-        user = Name.new('Tech', 'Jiro', 'Tech-Jiro@gmail.com', 'div')
-        expect(user.full_name).not_to eq('Tech Taro')
-      end
+    it 'フルネームでを返すこと' do
+      user = Name.new('Tech', 'Taro', 'Tech-Taro@gmail.com', 'div')
+      expect(user.full_name).to eq('Tech Taro')
     end
   end
 end
