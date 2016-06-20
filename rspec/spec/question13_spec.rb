c87e0971d341984bcff22f32389b723e76f1fddd
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-21 00:07:03 +0900

    Modify the order in which examples are written.

--- rspec/spec/question13_spec.rb
+++ rspec/spec/question13_spec.rb
@@ -1,16 +1,9 @@
 require 'question13'
 
 describe Run do
-  describe '#distance(distance)' do
-    it 'Tech Taroが走った距離を返すこと' do
-      run = build(:run, name: 'Tech Taro')
-      expect(run.distance(6)).to eq('Tech Taroさんは 6 km 走りました。')
-    end
-  end
-
   describe '#self.count' do
     it '走った人数を返すこと' do
-      run = build_list(:with_four_people, 4)
+      run = build_list(:with_four_people, 5)
       expect(Run.count).to eq(5)
     end
   end
@@ -24,6 +17,13 @@ describe Run do
   describe '#result' do
     it '結果を返すこと' do
       expect(Run.result).to eq('5人で、合計30 km 走りました。')
+    end
+  end
+
+  describe '#distance(distance)' do
+    it 'Tech Taroが走った距離を返すこと' do
+      run = build(:run, name: 'Tech Taro')
+      expect(run.distance(6)).to eq('Tech Taroさんは 6 km 走りました。')
     end
   end
 end
