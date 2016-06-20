b53672ffd341f02c0a6ad3d3d7a42c2436803115
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-20 23:02:49 +0900

    Simplify the code by deleting unnecessary fail test.

--- rspec/spec/question10_spec.rb
+++ rspec/spec/question10_spec.rb
@@ -3,15 +3,8 @@ require 'question10'
 describe Sort do
   describe '#self.sort_numbers' do
     let(:numbers_in_an_array) { build(:sort) }
-    context 'sort_numbersが正しい場合' do
-      it '昇順にソートされた配列になること' do
-        expect(numbers_in_an_array.sort_numbers).to match([0, 1, 2, 6, 9])
-      end
-    end
-    context 'sort_numbersが正しくない場合' do
-      it '昇順にソートされた配列にならないこと' do
-        expect(numbers_in_an_array.sort_numbers).not_to match([9, 6, 2, 1, 0])
-      end
+    it '昇順にソートされた配列になること' do
+      expect(numbers_in_an_array.sort_numbers).to match([0, 1, 2, 6, 9])
     end
   end
 end
