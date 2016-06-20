dc216ff9bc9ed603edb68a77fc4e5099c6ebbbbf
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-06-20 22:58:18 +0900

    Simplify the code by deleting unnecessary fail test.

--- rspec/spec/question08_spec.rb
+++ rspec/spec/question08_spec.rb
@@ -2,18 +2,9 @@ require 'question08'
 
 describe Fruit do
   describe '#my_fruits' do
-    context 'フルーツがりんごを含む場合' do
-      it '正しいこと' do
-        fruit = build(:fruit)
-        expect(fruit.my_fruits).to include('apple')
-      end
-    end
-
-    context 'フルーツがりんごを含まない場合' do
-      it '正しくないこと' do
-        fruit = build(:fruit, first_fruit: 'banana')
-        expect(fruit.my_fruits).not_to include('apple')
-      end
+    it '返り値にりんごを含むこと' do
+      fruit = build(:fruit)
+      expect(fruit.my_fruits).to include('apple')
     end
   end
 end
