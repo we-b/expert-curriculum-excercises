61bcbdae18293eab3be363839753d9937bcb3a2e
Daiki Nagaoka <Daiki@lneck.local>
2016-07-10 12:29:56 +0900

    Translate to question title in Japanese

--- Javascript Koans/koans/AboutInheritance.js
+++ Javascript Koans/koans/AboutInheritance.js
@@ -18,31 +18,31 @@ function SwedishChef(age, hobby, mood) {
 
 SwedishChef.prototype = new Muppet();
 
-describe("About inheritance", function() {
+describe("第7章 「クラスの継承」", function() {
   //このモジュール内において、他のメソッドが呼び出される前にthis.muppetメソッド、this.swedishメソッドが呼び出される。
   beforeEach(function(){
     this.muppet = new Muppet(2, "coding");
 	this.swedishChef = new SwedishChef(2, "cooking", "chillin");
   });
 
-  it("should be able to call a method on the derived object", function() {
+  it("40: 「メソッドによる派生オブジェクトの呼び出し」", function() {
     expect(this.swedishChef.cook()).toEqual(FILL_ME_IN);//Mmmm soup!
     //まず、beforeeachメソッドにより、この関数が実行される前にthis.swedishChef = new SwedishChefメソッドが呼ばれる。
     //SwedishChefクラスのプロパティにアクセスし、返り値として"Mmmm soup"が帰ってくる。
   });
 
-  it("should be able to call a method on the base object", function() {
+  it("41: 「メソッドによる基底オブジェクトの呼び出し」", function() {
     expect(this.swedishChef.answerNanny()).toEqual(FILL_ME_IN);//"Everything's cool!"
     //answeNannyメソッドはMuooetクラスのメソッドだが、SwedishChefはMuppetクラスをprototypeメソッドによって追加しているので、使う事ができます。（継承しているような状態)
   });
 
-  it("should set constructor parameters on the base object", function() {
+  it("42: 「基底オブジェクトのプロパティ」", function() {
     //SwedishChefクラスのインスタンスが作られた時に引数で2、"cooking"を渡しているのでageプロパティは2、hobyプロパティは"cooking"
     expect(this.swedishChef.age).toEqual(FILL_ME_IN);//2
     expect(this.swedishChef.hobby).toEqual(FILL_ME_IN);//"cooking"
   });
 
-  it("should set constructor parameters on the derived object", function() {
+  it("43: 「派生オブジェクトのプロパティ」", function() {
     expect(this.swedishChef.mood).toEqual(FILL_ME_IN);//"chillin"
   });
 });
@@ -66,26 +66,26 @@ function Gonzo(age, hobby, trick) {
 //no longer need to call the Muppet (base type) constructor
 Gonzo.prototype = Muppet.prototype.beget();
 
-describe("About Crockford's inheritance improvement", function() {
+describe("第8章 「ダグラス・クロックフォード流のクラスの継承を理解しよう」", function() {
   beforeEach(function(){
   this.gonzo = new Gonzo(3, "daredevil performer", "eat a tire");
   });
 
-  it("should be able to call a method on the derived object", function() {
+  it("44: 「メソッドによる派生オブジェクトの呼び出し」", function() {
     expect(this.gonzo.doTrick()).toEqual(FILL_ME_IN);//"eat a tire"
   });
 
-  it("should be able to call a method on the base object", function() {
+  it("45: 「メソッドによる基底オブジェクトの呼び出し」", function() {
     //gonzoも同じくMuooetを追加しているのでMupetオブジェクトのプロパティにアクセスできる。
     expect(this.gonzo.answerNanny()).toEqual(FILL_ME_IN);//"Everything's cool!"
   });
 
-  it("should set constructor parameters on the base object", function() {
+  it("46: 「基底オブジェクトのプロパティ」", function() {
     expect(this.gonzo.age).toEqual(FILL_ME_IN);//3
     expect(this.gonzo.hobby).toEqual(FILL_ME_IN);//"daredevil performer"
   });
 
-  it("should set constructor parameters on the derived object", function() {
+  it("47: 「派生オブジェクトのプロパティ」", function() {
     expect(this.gonzo.trick).toEqual(FILL_ME_IN);//"eat a tire"
   });
 });
