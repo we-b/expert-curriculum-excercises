0b15d8a07c0895e1dc8f867fddfb298bd108db4c
yamane yusuke <yusukeyamane01@gmail.com>
2016-07-29 23:36:30 +0900

    add filter code

--- js-application-basic/src/scripts/components/tweet-app.js
+++ js-application-basic/src/scripts/components/tweet-app.js
@@ -8,15 +8,48 @@ var MainHeader = React.createClass({
   }
 });
 
+var TweetForm = React.createClass({
+  render: function(){
+    return (
+      <div className="tweet-input">
+        <textarea className="tweet-input__textarea" name="tweet" cols="30" rows="10" placeholder="いま何してる？"></textarea>
+      </div>
+    );
+  }
+});
+
+var TweetList = React.createClass({
+  render: function(){
+    return (
+      <div className="tweets">
+      </div>
+    );
+  }
+});
+
+var FilterTweet = React.createClass({
+  render: function(){
+    return (
+      <ul className="filter__items">
+        <li className="filter__item current">全てのツイート</li>
+        <li className="filter__item">全てのツイート</li>
+      </ul>
+    );
+  }
+});
+
 var TweetApp = React.createClass({
-  render: function() {
+  render: function(){
     return (
       <div className="main">
         <div className="container">
           <MainHeader />
+          <TweetForm />
+          <TweetList />
+          <FilterTweet />
         </div>
       </div>
-      );
+    );
   }
 });
 
