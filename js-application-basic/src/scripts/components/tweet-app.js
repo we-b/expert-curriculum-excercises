f921868c45476a7b4d0f36001443b5c2073c460b
yamane yusuke <yusukeyamane01@gmail.com>
2016-07-30 07:54:49 +0900

    set todos

--- js-application-basic/src/scripts/components/tweet-app.js
+++ js-application-basic/src/scripts/components/tweet-app.js
@@ -1,3 +1,9 @@
+var TweetApp = React.createClass({
+  getInitialState: function() {
+      return {
+        todos: [{body:"hello", isFavaried:false}]
+      };
+  },
 var MainHeader = React.createClass({
   render: function(){
     return (
@@ -22,6 +28,17 @@ var TweetList = React.createClass({
   render: function(){
     return (
       <div className="tweets">
+        <section className="tweet">
+          <div className="profile">
+          <p className="user"><span className="user-icon lsf">user</span>名無しさん</p>
+          </div>
+          <p className="tweet__body">
+            hello word
+          </p>
+          <a className="js-favorite lsf-icon" title="star">
+            お気に入りに登録
+          </a>
+        </section>
       </div>
     );
   }
@@ -34,21 +51,6 @@ var FilterTweet = React.createClass({
         <li className="filter__item current">全てのツイート</li>
         <li className="filter__item">全てのツイート</li>
       </ul>
-    );
-  }
-});
-
-var TweetApp = React.createClass({
-  render: function(){
-    return (
-      <div className="main">
-        <div className="container">
-          <MainHeader />
-          <TweetForm />
-          <TweetList />
-          <FilterTweet />
-        </div>
-      </div>
     );
   }
 });
